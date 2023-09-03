f7fcd513acd3c004d741cec8e13d63a7a9f6c912
MityugovKonstantin <alxmira@yandex.ru>
2023-09-03 22:22:57 +0700

    Refactor code

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,51 +1,44 @@
-<script>
-  import BookWindow from './components/BookWindow.vue'
-  import DataService from './services/DataService.js'
+<script setup>
 
-  export default {
-  components: { BookWindow },
-    data() {
-      return {
-        books: [],
-        book: {
-          id: null,
-          name: null,
-          author: null
-        },
-        pressedId: 1
-      }
-    },
-    methods: {
-      getAll() {
-        DataService.getAll().then(response => this.books = response.data);
-      }, 
+import BookWindow from './components/BookWindow.vue'
+import DataService from './services/DataService.js'
+import { onMounted, ref } from 'vue';
 
-      showBook(id) {
-        this.pressedId = id;
-        console.log(id);
-      }
-    },
-    mounted() {
-      this.getAll();
-    }
-  }
+const books = ref([])
+const pressedId = ref(1)
+
+function getAll() {
+  DataService.getAll().then(response => books.value = response.data);
+}
+
+function showBook(id) {
+  this.pressedId = id;
+  console.log(id);
+}
+
+onMounted(() => getAll());
+
 </script>
 
 <template>
-  <div class="flex-container">
-    <ul class="list" v-if="books.length != 0">
-      <li class="field" v-for="book in books" :key="book.id">
-        <a @click="showBook(book.id)" href="#background-blackout">{{ book.author + ', ' + book.name }}</a>
-      </li>
-    </ul>
-  </div>
-  <BookWindow book-id="pressedId"></BookWindow>
+
+<div class="flex-container">
+  <ul class="list" v-if="books.length != 0">
+    <li class="field" v-for="book in books" :key="book.id">
+      <a @click="showBook(book.id)" href="#background-blackout">{{ book.author + ', ' + book.name }}</a>
+    </li>
+  </ul>
+</div>
+<BookWindow :book-id="pressedId.value"></BookWindow>
+
 </template>
 
 <style>
-  .flex-container {
-    display: flex;
-    flex-direction: column;
-    color: white;
-  }
+
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  color: white;
+}
+
 </style>
--- vue-project/src/components/BookWindow.vue
+++ vue-project/src/components/BookWindow.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+
+import { ref } from 'vue';
 import DataService from '/src/services/DataService.js'
 
 const props = defineProps({
@@ -17,56 +18,61 @@ function getBook(id) {
     DataService.getOne(id).then(response => book.value = response.data);
 }
 
-onMounted(() => getBook(props.bookId))
+// TODO:  Update book 
+//            Make something to update the book data in this component when it is called
 
 </script>
 
 <template>
-    <div id="background-blackout">
-      <div id="window">
-        {{ book }}
-        <a href="#" class="close">Close window</a>
-      </div>
-    </div>
+
+<div id="background-blackout">
+  <div id="window">
+    {{ book }}
+    <a href="#" class="close">Close window</a>
+  </div>
+</div>
+
 </template>
 
 <style>
-      #background-blackout {
-        background: rgba(102, 102, 102, 0.5);
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        display: none;
-      }
-      #window {
-        width: 50%;
-        height: 50%;
-        text-align: center;
-        padding: 15px;
-        border: 3px solid #0000cc;
-        border-radius: 10px;
-        color: #0000cc;
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        margin: auto;
-        background: #fff;
-      }
-      #background-blackout:target {display: block;}
-      .close {
-        display: inline-block;
-        border: 1px solid #0000cc;
-        color: #0000cc;
-        padding: 0 12px;
-        margin: 10px;
-        text-decoration: none;
-        background: #f2f2f2;
-        font-size: 14pt;
-        cursor:pointer;
-      }
-      .close:hover {background: #e6e6ff;}
-    </style>
+
+#background-blackout {
+  background: rgba(102, 102, 102, 0.5);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: none;
+}
+#window {
+  width: 50%;
+  height: 50%;
+  text-align: center;
+  padding: 15px;
+  border: 3px solid #0000cc;
+  border-radius: 10px;
+  color: #0000cc;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  background: #fff;
+}
+#background-blackout:target {display: block;}
+.close {
+  display: inline-block;
+  border: 1px solid #0000cc;
+  color: #0000cc;
+  padding: 0 12px;
+  margin: 10px;
+  text-decoration: none;
+  background: #f2f2f2;
+  font-size: 14pt;
+  cursor:pointer;
+}
+.close:hover {background: #e6e6ff;}
+
+</style>
